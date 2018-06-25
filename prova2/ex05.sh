5f34ac6f8752174add411e013bc33e904c01a40e
funck <funck@alunos.utfpr.edu.br>
2018-06-25 01:04:11 +0000

    Novos exemplos

--- prova2/ex05.sh
+++ prova2/ex05.sh
@@ -1,3 +1,3 @@
 # copiando os arquivos do /etc que começam com S e terminam com F. E os arquivos que começam com P e terminam com S.
 
-cp /etc/s*f /etc/p*s /home/funck/prova2/teste/
+cp /etc/* /home/funck/shell/prova2/| egrep '(conf$|^s)'
---
+++ prova2/ex07.sh
@@ -0,0 +1,2 @@
+#-------- Ler uma linhas especifica ----------------#
+ls -l| head -n 4| tail -n 1
